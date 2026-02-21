047e00b553fd6ab85211ad3b978290a8edb2a47b
NightKnightRen <kir.vishn@gmail.com>
2026-02-21 00:57:29 -0500

    dropout

--- lstm.py
+++ lstm.py
@@ -9,15 +9,17 @@ from sklearn.decomposition import PCA
 
 
 class myLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, out_size, dropout = 0.0, num_layers = 1):
+    def __init__(self, hidden_size, dropout = 0.0, num_layers = 1, input_size = 17, output_size = 17):
         super().__init__()
         self.LSTM = nn.LSTM(input_size, hidden_size, batch_first=True, dropout = dropout, num_layers = num_layers)
-        self.linear = nn.Linear(hidden_size, out_size)
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(hidden_size, output_size)
 
     def forward(self, input):
         output, (h_n, c_n) = self.LSTM(input)
 
         last_timestep = output[:, -1, :]
-        x = self.linear(last_timestep)
+        x = self.dropout(last_timestep)
+        x = self.linear(x)
         return x
 
