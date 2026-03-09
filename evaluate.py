3145002a4993243915e9768bb51ed85c73bbc22c
NightKnightRen <kir.vishn@gmail.com>
2026-03-09 17:08:25 -0400

    dataloader

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,16 @@
 
 import torch
 import numpy as np
+from torch.utils.data import DataLoader
 
 def evaluate_lstm_scores(model, forecasting_dataset):
+    loader = DataLoader(forecasting_dataset, batch_size=64, shuffle=False)
     scores, labels, categories = [], [], []
     with torch.no_grad():
-        for idx in range(len(forecasting_dataset)):
-            X, Y, label, category = forecasting_dataset[idx]
-            y_pred = model(X.unsqueeze(dim = 0))
-            error = torch.linalg.norm(Y - y_pred.squeeze(0)).item()
-            scores.append(error)
-            labels.append(label)
-            categories.append(category)
+        for X, Y, label, category in loader:
+            y_pred = model(X)
+            error = torch.linalg.norm(Y - y_pred, dim=-1) 
+            scores.extend(error.cpu().numpy())
+            labels.extend(label.cpu().numpy())
+            categories.extend(category)
     return np.array(scores), np.array(labels), np.array(categories)
